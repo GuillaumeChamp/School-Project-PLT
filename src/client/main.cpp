cab95f84565025fd86d8f32751987538913ca0f1
LNordine <nordine93190@gmail.com>
2023-10-02 16:45:26 +0200

    Ligne de commande Hello

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 // Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
@@ -17,12 +18,23 @@ void testSFML() {
 using namespace std;
 using namespace state;
 
-int main(int argc,char* argv[])
-{
-    Exemple exemple;
-    exemple.setX(53);
 
-    cout << "It works Simon!" << endl;
+int main(int argc, char *argv[]) {
+    
+    if (argc >= 2) {
+       
+        if (std::strcmp(argv[1], "hello") == 0) { //comparaisons des string
+           std::cout << "Coucou mon petit " << std::endl;
+
+        } else {
+            // erreur si mauvaise commande
+            std::cout << "Commande invalide. La seule commande est  ./bin/client hello" << std::endl;
+        }
+    } else {
+        // Si aucun argument n'est fourni, affichez un message d'erreur
+        std::cout << "Commande invalide. La seule commande est  ./bin/client hello" << std::endl;
+    }
 
     return 0;
 }
+
