f426ef4ec952b4c1a7546edde71c0a886802b9f5
Cinortas <simon.tristant2002@gmail.com>
2023-10-22 08:50:54 -0500

    getter et setter

--- src/shared/state/ActivePlayer.cpp
+++ src/shared/state/ActivePlayer.cpp
@@ -3,10 +3,18 @@
 //
 #include "ActivePlayer.h"
 
+class ActivePlayer{
+
+};
+
 namespace state {
 
     std::vector<Card> ActivePlayer::getHand(){
         return this->playerHand;
+    }
+
+    void ActivePlayer::setHand (std::vector<Card> hand){
+        this->playerHand = hand;
     }
 
 }
--- test/shared/test_shared_dummy.cpp
+++ test/shared/test_shared_dummy.cpp
@@ -1,19 +1,22 @@
 #include <boost/test/unit_test.hpp>
 
 #include "../../src/shared/state/ActivePlayer.h"
+#include "../../src/shared/state.h"
 
 using namespace ::state;
 
-BOOST_AUTO_TEST_CASE(TestStaticAssert)
-{
-  BOOST_CHECK(1);
-}
 
-BOOST_AUTO_TEST_CASE(TestExemple)
+BOOST_AUTO_TEST_CASE(TestState)
 {
   {
     ActivePlayer ex {};
     BOOST_CHECK_EQUAL(ex.getNameOfPlayer(), std::string());
+
+    Player plr ;
+    plr.setNumberOfCards(4)
+    BOOST_CHECK_EQUAL(plr.getNumberOfCards,4)
+    plr.setNumberOfCoins(5)
+    BOOST_CHECK_EQUAL(plr.getNumberOfCoins,5)
   }
 }
 
