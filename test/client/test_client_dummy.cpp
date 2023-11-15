0bb3da0bc9c43f6be6d18fe09b055a48a1bf5e53
guillaume <guillaume.champtoussel@ensea.fr>
2023-11-15 20:52:57 +0100

    adding better coverage

--- test/client/test_client_dummy.cpp
+++ test/client/test_client_dummy.cpp
@@ -20,6 +20,26 @@ BOOST_AUTO_TEST_CASE(TestSFML)
         BOOST_CHECK_EQUAL(button.getSurface().getPosition().x,10);
         BOOST_CHECK_EQUAL(button.getSurface().getPosition().y,20);
 
+        BOOST_CHECK_EQUAL(render::IHMState::getInstance()->hoverCard, nullptr);
+        BOOST_CHECK_EQUAL(render::IHMState::getInstance()->hoverButton, nullptr);
+        BOOST_CHECK_EQUAL(render::IHMState::getInstance()->isHandDisplayed, false);
+        BOOST_CHECK_EQUAL(render::IHMState::getInstance()->isHelpDisplayed, false);
+
+        state::Player playerA {"player1", state::PlayerId::PlayerA};
+        state::Player playerB {"player2", state::PlayerId::PlayerB};
+        state::Player playerC {"player3", state::PlayerId::PlayerC};
+        state::Player playerD {"player4", state::PlayerId::PlayerD};
+
+        state::GameState gameState {std::vector<state::Player>{playerA,playerB,playerC,playerD}};
+        render::Scene scene{render::SceneId::PlayerA,&gameState};
+
+        sf::RenderWindow render{};
+        sf::Event event{};
+        BOOST_CHECK_NO_THROW(scene.draw(render));
+        BOOST_CHECK_NO_THROW(scene.handleEvent(event));
+
+        sf::Font font{};
+        BOOST_CHECK_NO_THROW(render::PlayerRender::drawPlayer(render,&playerA,0,true,true,font));
     }
 }
 
