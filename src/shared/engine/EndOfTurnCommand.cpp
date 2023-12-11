d981c9d20381072adad0b19e38fd0339a5c26d7c
KarlLbs <karl.lubbos@ensea.fr>
2023-12-11 08:37:14 +0100

    EndOfTurnCommand

--- src/shared/engine/EndOfTurnCommand.cpp
+++ src/shared/engine/EndOfTurnCommand.cpp
@@ -5,6 +5,7 @@ namespace engine {
 
     // Constructor
     EndOfTurnCommand::EndOfTurnCommand(state::PlayerId authorPlayer) {
+        this->authorPlayer = authorPlayer;
     }
 
     // Destructor
@@ -13,7 +14,16 @@ namespace engine {
 
     // Execute method
     void EndOfTurnCommand::execute(state::GameState &state) {
+        // Switch to the next player
+        state::PlayerId currentPlayer = static_cast<state::PlayerId>(static_cast<int>(authorPlayer) + 1);
 
+        // Check if we reached the last player and wrap around to the first player
+        if (currentPlayer > state::PlayerD) {
+            currentPlayer = state::PlayerA;
+        }
+
+        // Updating the state
+        state.setPlaying(currentPlayer);
     }
 
     // Serialize method
