8fbd1d611a29844153077f5eacafa048af7e8380
guillaume <guillaume.champtoussel@ensea.fr>
2023-12-12 17:34:19 +0100

    adding ChangePhaseCommand

--- test/shared/test_shared_engine.cpp
+++ test/shared/test_shared_engine.cpp
@@ -76,8 +76,6 @@ BOOST_FIXTURE_TEST_SUITE(s, F)
 
         gameState.setPlaying(state::PLAYER_B);
 
-        state::Card card{"1", state::CardType::RELIGIOUS, 3};
-
         auto *command = new DrawCommand(state::PlayerId::PLAYER_A, 2);
         Engine *gameEngine = Engine::getInstance(gameState);
         BOOST_CHECK_EQUAL(gameState.getDrawableCards().size(), 0);
@@ -93,7 +91,7 @@ BOOST_FIXTURE_TEST_SUITE(s, F)
 
         plr1.setDrawAvailability(true);
         gameState.updatePlayer(plr1);
-        //Command not executed because already in the board
+
         BOOST_CHECK_EQUAL(command->check(gameState), true);
         BOOST_CHECK_EQUAL(gameState.getStack().size(), 0);
 
@@ -110,6 +108,44 @@ BOOST_FIXTURE_TEST_SUITE(s, F)
         BOOST_CHECK_EQUAL(gameState.getDrawableCards().size(), 3);
         BOOST_CHECK_EQUAL(gameState.getStack().size(), 60);
 
+    }
+
+    BOOST_AUTO_TEST_CASE(TestEndOfTurnCommand) {
+        state::Player plr1 = gameState.getPlayer(state::PLAYER_A);
+
+        gameState.setPlaying(state::PLAYER_A);
+
+        auto *command = new EndOfTurnCommand(state::PlayerId::PLAYER_A);
+        Engine *gameEngine = Engine::getInstance(gameState);
+        BOOST_CHECK_EQUAL(gameState.getPlaying(), state::PlayerId::PLAYER_A);
+        //Check serialized NIY
+        BOOST_CHECK_NO_THROW(command->serialize());
+
+        BOOST_CHECK_EQUAL(command->check(gameState), true);
+        BOOST_CHECK_EQUAL(gameState.getStack().size(), 0);
+
+        gameEngine->addCommand(command);
+        gameEngine->executeAllCommands();
+
+        BOOST_CHECK_EQUAL(gameState.getPlaying(), state::PlayerId::PLAYER_B);
+        command = new EndOfTurnCommand(state::PlayerId::PLAYER_B);
+        gameEngine->addCommand(command);
+        gameEngine->executeAllCommands();
+
+        BOOST_CHECK_EQUAL(gameState.getPlaying(), state::PlayerId::PLAYER_C);
+        command = new EndOfTurnCommand(state::PlayerId::PLAYER_C);
+        gameEngine->addCommand(command);
+        gameEngine->executeAllCommands();
+
+        BOOST_CHECK_EQUAL(gameState.getPlaying(), state::PlayerId::PLAYER_D);
+        command = new EndOfTurnCommand(state::PlayerId::PLAYER_D);
+        gameEngine->addCommand(command);
+        gameEngine->executeAllCommands();
+
+        BOOST_CHECK_EQUAL(gameState.getPlaying(), state::PlayerId::PLAYER_A);
+        command = new EndOfTurnCommand(state::PlayerId::PLAYER_A);
+        gameEngine->addCommand(command);
+        gameEngine->executeAllCommands();
     }
 
 BOOST_AUTO_TEST_SUITE_END()
