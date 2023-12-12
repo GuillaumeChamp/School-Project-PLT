fbc195c9c9b1bb79b2c1fcae0d470e7b9d95593a
KarlLbs <karl.lubbos@ensea.fr>
2023-12-12 15:11:50 +0100

    Reinstating ClaimBuildingGold and typo corrections

--- src/shared/engine/ClaimBuildingGold.cpp
+++ src/shared/engine/ClaimBuildingGold.cpp
@@ -1,27 +1,71 @@
 // ClaimBuildingGold.cpp
 #include "ClaimBuildingGold.h"
 
+
 namespace engine {
 
-    // Constructor
-    ClaimBuildingGold::ClaimBuildingGold(state::PlayerId authorPlayer) {
-        this->authorPlayer = authorPlayer;
+  // Constructor
+  ClaimBuildingGold::ClaimBuildingGold(state::PlayerId authorPlayer) {
+    this->authorPlayer=authorPlayer;
+  }
+
+  // Destructor
+  ClaimBuildingGold::~ClaimBuildingGold() {
+  }
+
+  // Execute method
+  void ClaimBuildingGold::execute(state::GameState& state) {
+    // Getting the player to execute the command on
+    state::Player player = state.getPlayer(authorPlayer);
+
+    // Getting his board, coins and character
+    std::vector<state::Card> board = player.getBoardOfPlayer();
+    int coins = player.getNumberOfCoins();
+    state::CharacterType character = player.getCharacter();
+
+
+    //Checking how many cards on his board match his character's color and adding coins accordingly
+    if (character == state::CharacterType::KING)
+    {
+      for (state::Card card : board){
+        if (card.getColorOfCard() == state::CardType::NOBLE) {
+          coins++;
+        }
+      }
+    }
+    else if (character == state::CharacterType::BISHOP)
+    {
+      for (state::Card card : board){
+        if (card.getColorOfCard() == state::CardType::RELIGIOUS) {
+          coins++;
+        }
+      }
+    }
+    else if (character == state::CharacterType::MERCHANT)
+    {
+      for (state::Card card : board){
+        if (card.getColorOfCard() == state::CardType::COMMERCIAL) {
+          coins++;
+        }
+      }
+    }
+    else if (character == state::CharacterType::WARLORD)
+    {
+      for (state::Card card : board){
+        if (card.getColorOfCard() == state::CardType::MILITARY) {
+          coins++;
+        }
+      }
     }
 
-    // Destructor
-    ClaimBuildingGold::~ClaimBuildingGold() {
-    }
+    // Setting the player's new number of coins
+    player.setNumberOfCoins(coins);
+    state.updatePlayer(player);
+  }
 
-    // Execute method
-    void ClaimBuildingGold::execute(state::GameState &state) {
-    }
+  // Serialize method
+  void ClaimBuildingGold::serialize() {
+  }
 
-    // Serialize method
-    void ClaimBuildingGold::serialize() {
-    }
-
-    bool ClaimBuildingGold::check(state::GameState &state) {
-        return Command::check(state);
-    }
 
 } // namespace engine
