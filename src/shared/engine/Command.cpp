14ade4fdbf01a0ef55adc8844f441b6e31b94355
guillaume <guillaume.champtoussel@ensea.fr>
2023-12-26 14:53:44 +0100

    cleaning command

--- src/shared/engine/Command.cpp
+++ src/shared/engine/Command.cpp
@@ -1,29 +1,27 @@
 // Command.cpp
 #include "Command.h"
 
-namespace engine {
+using namespace engine;
 
-    // Constructor
-    Command::Command(){
-        this->targetPlayer=state::NO_PLAYER;
-        this->authorPlayer=state::NO_PLAYER;
-        this->commandTypeId =CommandTypeId::NO_COMMAND;
-    };
+// Constructor
+Command::Command() {
+    this->targetPlayer = state::NO_PLAYER;
+    this->authorPlayer = state::NO_PLAYER;
+    this->commandTypeId = CommandTypeId::NO_COMMAND;
+}
 
-    // Destructor
-    Command::~Command() = default;
+// Destructor
+Command::~Command() = default;
 
-    // Getter for CommandTypeId
-    CommandTypeId Command::getCommandTypeId() {
-        return commandTypeId;
-    }
+// Getter for CommandTypeId
+CommandTypeId Command::getCommandTypeId() {
+    return commandTypeId;
+}
 
-    // Execute method
-    void Command::execute(state::GameState &state) {}
+// Execute method
+void Command::execute(state::GameState &state) {}
 
-    bool Command::check(state::GameState &state) {
-        return authorPlayer==state.getPlaying();
-    }
+bool Command::check(state::GameState &state) {
+    return authorPlayer == state.getPlaying();
+}
 
-
-} // namespace engine
