dee8643fac0a78177ad9ec97c6ca7ac749216f43
guillaume <guillaume.champtoussel@ensea.fr>
2023-12-31 17:12:16 +0100

    finishing LiveGame logic

--- src/server/server/LiveGame.cpp
+++ src/server/server/LiveGame.cpp
@@ -19,18 +19,26 @@ LiveGame &LiveGame::getInstance() {
 void LiveGame::addCommand(std::string command) {
     distributedCommands.addCommand(command);
     //TODO : create command using an engine util method
-    engine::Command* command1 = new engine::Command();
+    engine::Command *command1 = new engine::Command();
     eng->addCommand(command1);
     eng->executeAllCommands();
 }
 
 string LiveGame::handlePlayerJoin(std::string playerName) {
-    state::PlayerId playerId = findPlayer(players,playerName);
-    if (players.size()>=4){
+    state::PlayerId playerId = findPlayer(players, playerName);
+    if (players.size() >= 4) {
         return "This game is full\r\n";
     }
-    if (playerId==state::NO_PLAYER){
-        players.emplace_back(playerName,static_cast<state::PlayerId>(players.size()+1));
+    if (playerId == state::NO_PLAYER) {
+        players.emplace_back(playerName, static_cast<state::PlayerId>(players.size() + 1));
+        if (players.size() == 4) {
+            this->game = new state::GameState{players[0].getClientName(), players[1].getClientName(),
+                                              players[2].getClientName(), players[3].getClientName()};
+            this->eng = std::addressof(engine::Engine::getInstance(*game));
+            //TODO use start game command
+            return "OK, game is starting.\r\n";
+
+        }
         return "OK\r\n";
     }
     return "You are already in the game\r\n";
@@ -41,8 +49,8 @@ state::GameState *LiveGame::getState() {
 }
 
 string LiveGame::retrieveCommands(string playerName) {
-    for (auto p : players){
-        if(p.getClientName()==playerName){
+    for (auto p: players) {
+        if (p.getClientName() == playerName) {
             distributedCommands.retrieveCommands(p.getLastUpdate());
         }
     }
